71cf569000cc263a8299330fc7abdc307847e4dd
EnhanceKen <117061529+EnhanceKen@users.noreply.github.com>
2025-03-21 11:41:10 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_tKeDUd1V1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1nqt1bhh3kljjnq35abhu64lhp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_7oXJq2omd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7ks5nhbkn03k7k57pttb08djqh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
